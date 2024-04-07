c83f42b7b77ae81f704df37bcea563258f8cc6e5
Konstanin <youmobilcom@gmail.com>
2024-04-07 20:45:55 +0300

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -23,17 +21,16 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+        "███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓",
+        "██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+       "▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░",
+       "▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██ ",
+       "▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒",
+       "░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░",
+       "░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░",
+       "  ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░   ",
+       "         ░    ░  ░    ░ ░        ░   ░         ░  ",
+       "                               ░                  ",
       }
       return opts
     end,
@@ -52,7 +49,13 @@ return {
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
-
+  {
+    "navarasu/onedark.nvim",
+    opts = function()
+      require("onedark").setup { style = "deep" }
+      require("onedark").load()
+    end,
+  },
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
@@ -81,5 +84,337 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+  { "NvChad/nvim-colorizer.lua", enabled = false },
+  {
+    "uga-rosa/ccc.nvim",
+    event = { "User AstroFile", "InsertEnter" },
+    cmd = { "CccPick", "CccConvert", "CccHighlighterEnable", "CccHighlighterDisable", "CccHighlighterToggle" },
+    keys = {
+      { "<leader>uC", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle colorizer" },
+      { "<leader>zc", "<cmd>CccConvert<cr>", desc = "Convert color" },
+      { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick Color" },
+    },
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    },
+    config = function(_, opts)
+      require("ccc").setup(opts)
+      if opts.highlighter and opts.highlighter.auto_enable then vim.cmd.CccHighlighterEnable() end
+    end,
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "swift" })
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      servers = { "sourcekit" },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "stevearc/dressing.nvim",
+    opts = {},
+  },
+  {
+    "tami5/xbase",
+    run = "make insatall",
+    dependencies = { "nvim-lua/plenary.nvim, nvim-telescope/telescope.nvim" },
+    opts = function() require("xbase").setup {} end,
+  },
+  {
+    "xbase-lab/xbase",
+    ft = { "swift", "objcpp", "objc" },
+    run = "make install",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim", optional = true },
+      { "nvim-lua/plenary.nvim", optional = true },
+      { "stevearc/dressing.nvim", optional = true }, -- (in case you don't use telescope but something else)
+    },
+    mappings = {
+      --- Whether xbase mapping should be disabled.
+      enable = true,
+      --- Open build picker. showing targets and configuration.
+      build_picker = "<leader>bp", --- set to 0 to disable
+      --- Open run picker. showing targets, devices and configuration
+      run_picker = "<leader>rp", --- set to 0 to disable
+      --- Open watch picker. showing run or build, targets, devices and configuration
+      watch_picker = "<leader>wp", --- set to 0 to disable
+      --- A list of all the previous pickers
+      all_picker = "<leader>ap", --- set to 0 to disable
+      --- horizontal toggle log buffer
+      toggle_split_log_buffer = "<leader>ls",
+      --- vertical toggle log buffer
+      toggle_vsplit_log_buffer = "<leader>lv",
+    },
+    statusline = {
+      watching = { icon = "", color = "#1abc9c" },
+      device_running = { icon = "", color = "#4a6edb" },
+      success = { icon = "", color = "#1abc9c" },
+      failure = { icon = "", color = "#db4b4b" },
+    },
+    --- Simulators to only include.
+    --- run `xcrun simctl list` to get a full list of available simulator
+    --- If the list is empty then all simulator available  will be included
+    simctl = {
+      iOS = {
+        -- "iPhone 13 Pro", --- only use this devices
+      },
+      watchOS = {}, -- all available devices
+      tvOS = {}, -- all available devices
+    },
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    opts = function() require("flutter-tools").setup {} end,
+  },
+  -- Add "flutter" extension to "telescope"
+  { import = "astrocommunity.pack.yaml" },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      handlers = { dartls = false },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart" })
+      end
+    end,
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    ft = "dart",
+    opts = function(_, opts)
+      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
+      if astrolsp_avail then opts.lsp = astrolsp.lsp_opts "dartls" end
+      opts.debugger = { enabled = true }
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dart" })
+        end,
+      },
+      {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+          opts.ensure_installed =
+            require("astrocore").list_insert_unique(opts.ensure_installed, { "dart-debug-adapter" })
+        end,
+      },
+    },
+  },
+  -- Add "flutter" extension to "telescope"
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = function() require("telescope").load_extension "flutter" end,
+  },
+    -- CSharp support
+    {
+      "nvim-treesitter/nvim-treesitter",
+      optional = true,
+      opts = function(_, opts)
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "c_sharp" })
+        end
+      end,
+    },
+    {
+      "jay-babu/mason-null-ls.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "csharpier" })
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "csharp_ls" })
+      end,
+    },
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "coreclr" })
+      end,
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = require("astrocore").list_insert_unique(
+          opts.ensure_installed,
+          { "csharp-language-server", "csharpier", "netcoredbg" }
+        )
+      end,
+    },
+  -- add support C++ cpp
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      config = {
+        clangd = {
+          capabilities = {
+            offsetEncoding = "utf-8",
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "cpp", "c", "objc", "cuda", "proto" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd" })
+    end,
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          clangd_extensions = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require "clangd_extensions"
+                  vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "Civitasv/cmake-tools.nvim",
+    ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    dependencies = {
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+        end,
+      },
+    },
+    opts = {},
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd", "codelldb" })
+    end,
+  },
+  -- add support CMAKE
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cmake" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "neocmake" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "neocmakelsp" })
+    end,
+  },
+  --add telescope-coc-nvim
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "fannheyward/telescope-coc.nvim" },
+    opts = function(_, opts)
+      require("telescope").load_extension "coc"
+      if not opts.extensions then opts.extensions = {} end
+      opts.extensions.coc = {
+        theme = "ivy",
+        prefer_locations = true, -- always use Telescope locations to preview definitions/declarations/implementations etc
+      }
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-live-grep-args.nvim" },
+    opts = function() require("telescope").load_extension "live_grep_args" end,
   },
 }

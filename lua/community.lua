318218cec8e88a806926b5bee0e12f9a0870a5e7
Konstanin <youmobilcom@gmail.com>
2024-04-07 20:48:01 +0300

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -8,5 +8,14 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.swift" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.kotlin" },
   -- import/override with your plugins folder
 }

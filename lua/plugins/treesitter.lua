9a5799b1c626f0c706544a8e98a2e415f45e2b84
Konstanin <youmobilcom@gmail.com>
2024-11-29 19:55:11 +0300

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,15 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "cmake",
       "swift",
+      "c_sharp",
+      "python",
+      "toml",
+      "java",
+      "html",
+      "kotlin",
+      "css"
       -- add more arguments for adding more treesitter parsers
     })
   end,

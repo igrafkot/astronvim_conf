f7f493f134f90ad73400c1e92ac12bafd65c2deb
Konstanin <youmobilcom@gmail.com>
2025-04-05 10:36:17 +0300

    Update treesitter.lua
    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
       "cpp",
       "c",
       "objc",
-      "cuda",
       "proto",
-      "vim",
       "cmake",
       "swift",
       "c_sharp",
       "python",
-      "toml",
       "java",
       "html",
       "kotlin",
-      "css"
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+      "css",
+      "json",
+      "jsonc",
+      "rust",
+      "toml",
+      "sql",
+    },
+  },
 }
